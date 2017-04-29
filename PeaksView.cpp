6d1238ee0145fa18ccec97650772c24f9680912d
dridk <sacha@labsquare.org>
2017-04-29 15:49:33 +0200

    remove wrong code , command all

--- PeaksView.cpp
+++ PeaksView.cpp
@@ -10,7 +10,7 @@ PeaksView::PeaksView(QWidget * parent)
 
     mView->setChart(mChart);
 
-    // setDragMode(QGraphicsView::RubberBandDrag);
+    //enable zoom mouse selection
     mView->setRubberBand( QChartView::HorizontalRubberBand);
     mChart->setBackgroundRoundness(0);
     QVBoxLayout * cLayout = new QVBoxLayout;
@@ -19,7 +19,6 @@ PeaksView::PeaksView(QWidget * parent)
     cLayout->setSpacing(0);
 
     setLayout(cLayout);
-
     setMaximumHeight(300);
 
 }
@@ -29,151 +28,74 @@ void PeaksView::setFilename(const QString &filename)
 {
 
     mFilename = filename;
-
     draw();
 
 }
 
 void PeaksView::rangeChanged(qreal min, qreal max)
 {
-    if (scrolling) return;  // Scrolling causes range changes, but we don't have to do anything.
+    //This methods is trigger during zoom mouse action
 
-    QChart          *c = mChart;
-    QAbstractAxis   *x = ax;
-    qreal            avg = (min + max) / 2.0;
-    bool             range_fixed = false;
-
-    /*
-        * Make sure the new range is sane; fix if not.
-        */
-    if ((max - min) < 0.1) {    // Avoid overzooming
-        min = avg - 0.05;
-        max = avg + 0.05;
-        range_fixed = true;
-    }
-
-    if (min < 0.0) { min = 0.0; range_fixed = true; }
-
-    if (max > 4.0) { max = 4.0; range_fixed = true; }
-
-    if (range_fixed) {
-        x->setRange( min, max );    // will re-signal with the fixed range
-        return;
-    }
-
-    qreal    vis_width = c->plotArea( ).width( );
-    qreal    all_width = vis_width * (4.0 - 0.0) / (max - min);
-
-    //    cerr << "range " << min << " ... " << max << " in " << vis_width << " pixels" << endl;
-    //    cerr << "full width requires " << all_width << " pixels" << endl;;
-
-    if (max - min < 4.0) {
-        //        cerr << "set scroll parameters" << endl;
-        scrolling = true;
-        mScrollBar->setMaximum( all_width - vis_width );
-        sv = min / (4.0 - 0.0) * all_width;
-        mScrollBar->setValue( sv );
-        scrolling = false;
-    } else {
-        //        cerr << "disable scroll bar" << endl;
-        scrolling = true;
-        mScrollBar->setMaximum( 0 );
-        mScrollBar->setValue( sv );
-        scrolling = false;
-    }
+    qDebug()<<min<<" "<<max;
 
 }
 
 void PeaksView::scrollChanged(int v)
 {
-    if (!scrolling) {
-          scrolling = true;
-  //      cerr << "scroll " << v << endl;
-          mChart->scroll( v - sv, 0 );
-          sv = v;
-          scrolling = false;
-      }
+    //this methods is triggered by the scrollbar
+    qDebug()<<v;
+
 }
 
 void PeaksView::draw()
 {
+    // remove old series
     mChart->removeAllSeries();
     mSeries.clear();
 
+    // parse ab1 file
     AbifReader reader(mFilename);
 
 
-    for (QString k : reader.directoryKeys())
-    {
-        if (reader.data(k).type() == QVariant::List)
-        {
-            qDebug()<<k<<" "<<reader.data(k).toList().size()<<" "<<reader.data(k).toList().mid(0,5);
-        }
-
-        else
-            qDebug()<<k<<" "<<reader.data(k);
-
-        if (reader.data(k).type() == QVariant::String)
-        {
-            qDebug()<<reader.data(k).toString().size();
-        }
-
-
-
-    }
-
-
-    // AB1 SPECIFICATION TO UNDERSTAND KEY
-    // get A,C,G,T signals
+    // loop over 4 series A,C,T,G
     for (int i=9; i<=12 ; ++i)
     {
+        // create DATA.x key
         QString key = QString("DATA.%1").arg(i);
 
 
-
-
-
+        // if key exists in ab1 file
         if (reader.directoryKeys().contains(key))
         {
+            // create a line serie
             QLineSeries * serie = new QLineSeries;
-            QScatterSeries * test = new QScatterSeries;
-
+            // get data according key
             QVariantList datas = reader.data(key).toList();
 
-            QVariantList peaks = reader.data("PLOC.1").toList();
-            QString text = reader.data("PBAS.1").toString();
-
-
-            qDebug()<<"last "<<peaks.last();
-
-
-
-
+            // fill serie with x,y values
             for (int i=0; i<datas.length(); ++i)
                 serie->append(i, datas[i].toInt());
 
-            for (int i=0; i<peaks.length(); ++i){
-                test->append(peaks[i].toInt(),1000 );
-
-            }
-
-
-
-
-
+            // add serie in the chart
             mChart->addSeries(serie);
-            mChart->addSeries(test);
         }
 
     }
 
-
+    // Create defaut axis X and Y
     mChart->createDefaultAxes();
+
+    // store axisX pointer as class member
     ax = qobject_cast<QValueAxis*>(mChart->axisX());
+
+    // set a defaut range to sanger plot readable
     ax->setRange(1500,2000);
 
 
+    // connect axis scale changed to class methods
     connect(ax, SIGNAL(rangeChanged(qreal,qreal)),this,SLOT(rangeChanged(qreal,qreal)));
+
+    // connect scrollbar to class methods
     connect(mScrollBar,SIGNAL(valueChanged(int)),this,SLOT(scrollChanged(int)));
 
 }
