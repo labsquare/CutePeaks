a41ef05ad475c61fe8613b1a3f9205881b4df696
dridk <sacha@labsquare.org>
2017-06-14 22:30:48 +0200

    trying but unsuccess to make a nice Fasta editor

--- sequenceview.cpp
+++ sequenceview.cpp
@@ -1,16 +1,28 @@
 #include "sequenceview.h"
 
 SequenceView::SequenceView(QWidget * parent)
-    :QTextEdit(parent)
+    :QPlainTextEdit(parent)
 {
 
     setReadOnly(true);
+    connect(this, &SequenceView::blockCountChanged, this, &SequenceView::updateFoo);
+
+
+
+    setViewportMargins(20,0,0,0);
 
 }
 
 void SequenceView::setSequence(const Sequence &sequence)
 {
     mSequence = sequence;
-    setText(sequence.byteArray());
+    setPlainText(sequence.byteArray());
+}
+
+
+
+void SequenceView::updateFoo(int v)
+{
+    qDebug()<<v;
 }
 
--- sequenceview.h
+++ sequenceview.h
@@ -3,11 +3,16 @@
 #include <QtWidgets>
 #include "sequence.h"
 
-class SequenceView : public QTextEdit
+class SequenceView : public QPlainTextEdit
 {
+    Q_OBJECT
 public:
     SequenceView(QWidget * parent = 0);
     void setSequence(const Sequence& sequence);
+
+
+protected Q_SLOTS:
+    void updateFoo(int v);
 
 
 
