2e9f659980055e4d158228a4e1014fbcc81aa8ca
dridk <sacha@labsquare.org>
2017-06-24 19:47:07 +0200

    update version fiel

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     a.setApplicationName("CutePeaks");
     a.setOrganizationName("Labsquare");
     a.setOrganizationDomain("Labsquare.org");
-    a.setApplicationVersion("0.1-beta");
+    a.setApplicationVersion("0.1");
 
 
     MainWindow w;
