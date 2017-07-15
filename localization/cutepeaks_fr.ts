8fdf25afe59890c607015cf9076449942e71ffce
Anne-Sophie Denommé-Pichon <asdeno@hotmail.fr>
2017-07-15 16:42:36 +0200

    French translation with QtLinguist

---
+++ localization/cutepeaks_fr.ts
@@ -0,0 +1,63 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../gui/aboutdialog.cpp" line="37"/>
+        <source>About %1</source>
+        <translation>À propos de %1</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../gui/MainWindow.cpp" line="61"/>
+        <source>Open Image</source>
+        <translation>Ouvrir un fichier</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../core/AbifSequenceTrace.cpp" line="55"/>
+        <source>Not an ABIF FILE</source>
+        <translation>N&apos;est pas un fichier ABIF (.ab1)</translation>
+    </message>
+    <message>
+        <location filename="../core/SequenceTraceFactory.cpp" line="20"/>
+        <source>Cannot open file</source>
+        <translation>Impossible d&apos;ouvrir le fichier</translation>
+    </message>
+    <message>
+        <location filename="../core/sequence.cpp" line="31"/>
+        <source>cannot complement a protein</source>
+        <translation>Impossible d&apos;obtenir le reverse complément de la protéine. Choisir une séquence nucléotidique</translation>
+    </message>
+    <message>
+        <location filename="../core/sequence.cpp" line="59"/>
+        <source>cannot translate a protein</source>
+        <translation>Impossible de traduire une protéine</translation>
+    </message>
+    <message>
+        <location filename="../core/sequence.cpp" line="142"/>
+        <source>dna</source>
+        <translation>ADN</translation>
+    </message>
+    <message>
+        <location filename="../core/sequence.cpp" line="143"/>
+        <source>rna</source>
+        <translation>ARN</translation>
+    </message>
+    <message>
+        <location filename="../core/sequence.cpp" line="144"/>
+        <source>protein</source>
+        <translation>Protéine</translation>
+    </message>
+    <message>
+        <location filename="../core/sequence.cpp" line="145"/>
+        <source>unknown</source>
+        <translation>Non connu</translation>
+    </message>
+</context>
+</TS>
